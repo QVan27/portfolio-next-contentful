8076a3053061369adf3b145fdcb6c3a7b80c2037
Quentin Vannarath <qvannarath@gmail.com>
2023-10-12 12:56:48 +0200

    🔨 [GRID] Added grid view to check if integration is correct

--- src/layouts/wrapper.js
+++ src/layouts/wrapper.js
@@ -1,9 +1,11 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import Grid from '@/components/Grid';
 
 const Wrapper = ({ children }) => {
   return (
     <>
+      <Grid />
       <Header />
       <main>
         <div>{children}</div>
