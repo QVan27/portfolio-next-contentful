865f72c04a33146386c7505094677a712216df77
Quentin Vannarath <qvannarath@gmail.com>
2023-10-13 13:16:19 +0200

    ✨ [FLARE-CURSOR] Template - Styles - RWD

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import Wrapper from "@/layouts/wrapper";
+import FlareCursor from '@/components/FlareCursor';
 import "@/styles/globals.css";
 
 
 export default function App({ Component, pageProps }) {
   return (
     <Wrapper>
+      <FlareCursor />
       <Component {...pageProps} />
     </Wrapper>
   );
