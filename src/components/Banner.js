f1147a58eaa20e4121e692fcbbb137e1a34b8119
Quentin Vannarath <qvannarath@gmail.com>
2023-10-17 14:07:35 +0200

    ✨ [BANNER] Connect to Contentful and added datas

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,9 @@
 import React, { useRef, useEffect } from 'react'
 import styled from 'styled-components'
 import { Anton, Maitree } from 'next/font/google'
-import Profile from "@/assets/images/quentin.jpeg"
 import Texture from "@/assets/images/distortions/texture.jpeg"
 import hoverEffect from 'hover-effect';
+import RichText from '@/components/RichText'
 
 const anton = Anton({
   weight: ["400"],
@@ -53,37 +53,42 @@ const Container = styled.section`
   }
 
   .texts {
-    p {
-      font-size: 1.5rem;
-      font-style: normal;
-      line-height: normal;
-      text-align: center;
+    div {
       width: min(46.31rem, 100%);
-      
-      @media screen and (min-width: 768px) { font-size: 2.5rem; }
-      
-      @media screen and (min-width: 1280px) { text-align: right; }
-      
+
       @media screen and (min-width: 1536px) { width: min(58.31rem, 100%); }
 
-      span { color: var(--highlight); }
+      p {
+        font-size: 1.5rem;
+        font-style: normal;
+        line-height: normal;
+        text-align: center;
+        
+        @media screen and (min-width: 768px) { font-size: 2.5rem; }
+        
+        @media screen and (min-width: 1280px) { text-align: right; }      
+  
+        b { color: var(--highlight); }
+      }
     }
 
     span { color: var(--highlight); }
   }
 `;
 
-export default function Banner() {
+export default function Banner({ data }) {
   const distortion = useRef(null)
+  const images = data.imagesDistortions
+  const imageDistortion1 = images[0].fields.file.url
+  const imageDistortion2 = images[1].fields.file.url
 
   // https://github.com/robin-dela/hover-effect
-
   useEffect(() => {
     new hoverEffect({
       parent: distortion.current,
       intensity: 0.2,
-      image1: Profile.src,
-      image2: Profile.src,
+      image1: imageDistortion1,
+      image2: imageDistortion2,
       displacementImage: Texture.src,
       imagesRatio: 280 / 200
     })
@@ -96,12 +101,14 @@ export default function Banner() {
       </div>
       <div className='min-h-screen grid grid-cols-12 xl:grid-cols-24 gap-x-2.5 items-center'>
         <h1 className='col-start-2 col-end-12 xl:col-start-3 xl:col-end-23 flex flex-col content'>
-          <span className={`${anton.className} text-center title-border`}>DEVELOPER</span>
+          <span className={`${anton.className} text-center title-border`}>{data.firstBigText}</span>
           <div className='flex flex-wrap justify-center xl:gap-x-12 texts lg:items-center'>
-            <p className={`${maitree.className}`}>Passionate about the world of the web and applications, I can be a <span>source of ideas</span> to bring all your projects to fruition.</p>
-            <span className={`${anton.className} title-full text-center`}>WEB</span>
+            <div className={`${maitree.className}`}>
+              <RichText content={data.contents} />
+            </div>
+            <span className={`${anton.className} title-full text-center`}>{data.secondBigText}</span>
           </div>
-          <span className={`${anton.className} hidden xl:block text-center title-border`}>WEB MOBILE</span>
+          <span className={`${anton.className} hidden xl:block text-center title-border`}>{data.thirdBigText}</span>
         </h1>
       </div>
     </Container>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,22 @@
 import Image from 'next/image'
+import { client } from '@/lib/contentful'
 import Banner from '@/components/Banner'
 
-export default function Home() {
+export default function Home({ dataBanner }) {
   return (
     <>
-      <Banner />
+      <Banner data={dataBanner[0].fields} />
     </>
   )
+}
+
+export const getStaticProps = async () => {
+  const banner = await client.getEntries({ content_type: 'banner' })
+
+  return {
+    props: {
+      dataBanner: banner.items,
+      revalidate: 70
+    }
+  }
 }
