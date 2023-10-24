a9d0e05017fbda05b9e4a18c88a336de31444e83
Quentin Vannarath <qvannarath@gmail.com>
2023-10-24 12:15:13 +0200

    ✨ [WORK] Template - Styles - RWD

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,15 @@ import { client } from '@/lib/contentful'
 import Banner from '@/components/Banner'
 import About from '@/components/About'
 import Stack from '@/components/Stack'
+import Work from '@/components/Work'
 
-export default function Home({ dataBanner, dataAbout, dataStack }) {
+export default function Home({ dataBanner, dataAbout, dataStack, dataWork, dataWebsite }) {
   return (
     <>
       <Banner data={dataBanner[0].fields} />
       <Stack data={dataStack[0].fields} />
       <About data={dataAbout[0].fields} />
+      <Work data={dataWork[0].fields} items={dataWebsite} />
     </>
   )
 }
@@ -17,12 +19,16 @@ export const getStaticProps = async () => {
   const banner = await client.getEntries({ content_type: 'banner' })
   const about = await client.getEntries({ content_type: 'about' })
   const stack = await client.getEntries({ content_type: 'stack' })
+  const work = await client.getEntries({ content_type: 'work' })
+  const website = await client.getEntries({ content_type: 'website' })
 
   return {
     props: {
       dataBanner: banner.items,
       dataAbout: about.items,
       dataStack: stack.items,
+      dataWork: work.items,
+      dataWebsite: website.items,
       revalidate: 70
     }
   }
