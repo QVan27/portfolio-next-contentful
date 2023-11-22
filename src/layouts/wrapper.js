a8d16e78f67b572f3a960f8fa21e580e6e3ad3cc
Quentin Vannarath <qvannarath@gmail.com>
2023-11-22 14:22:46 +0100

    🔥 [wrapper] Remove Grid component from wrapper layout

--- src/layouts/wrapper.js
+++ src/layouts/wrapper.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import Grid from '@/components/Grid';
 import Loader from '@/components/Loader';
 
 const Wrapper = ({ children }) => {
   return (
     <>
-      <Grid />
       <Loader />
       {children}
     </>
